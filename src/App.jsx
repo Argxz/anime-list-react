0c8c5273b41e1a7e3e23ab94c01664d34af7f713
Argxz <riyanargaw@gmail.com>
2024-10-30 23:17:23 +0700

    dashboard setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import './App.css'
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import List from './pages/List';
+import About from './pages/About';
 
 function App() {
-  return <div></div>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/list-anime" element={<List />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,45 @@
+import '../App.css'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Dashboard() {
+    const navigate = useNavigate();
+
+    const recommendedAnime = [
+        { name: 'Blue Lock Season 2', rating: 2 },
+        { name: 'Re:Zero kara Season 3', rating: 3 },
+        { name: 'SAO : Gun Gale Online 2', rating: 3 },
+        { name: 'Dragon Ball Daima', rating: 5 },
+    ];
+
+    const renderStars = (rating) => {
+        return '★'.repeat(rating) + '☆'.repeat(5 - rating);
+    };
+
+    return (
+    <div className="dashboard-container">
+            <h1>Selamat Datang di AnimeKU</h1>
+            <p>Temukan, eksplorasi, dan lacak anime favoritmu!</p>
+            <p>Jadikan pengalaman menonton anime-mu lebih menyenangkan dan seru!</p>
+
+
+        <div className="button-container">
+            <button onClick={() => navigate('/anime-list')}>List Anime</button>
+            <button onClick={() => navigate('/about')} style={{ marginLeft: '10px' }}>Tentang</button>
+        </div>
+
+        <div className="recommendations">
+            <h2>Anime Terbaru 2024</h2>
+            <ul>
+                {recommendedAnime.map((anime, index) => (
+                <li key={index}>
+                    {anime.name} <span className="stars">{renderStars(anime.rating)}</span>
+                </li>
+                ))}
+            </ul>
+        </div>
+    </div>
+    );
+}
+
+export default Dashboard;
